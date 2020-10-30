7516994c9e92086411760da8f676ec34aabb969f
Owuor Robert <owuorrobert76@gmail.com>
2020-10-30 14:46:43 +0300

    CREATED PRODUCT LIST COMPONENT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { templateJitUrl } from '@angular/compiler';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector :'pm-root',
+  template :`
+    <div><h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+    `
+
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent{
+  pageTitle:string = 'Acme Product Management'
 }
